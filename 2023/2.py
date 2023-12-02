b253ae6493a7d15d796d9170e7e3c9e496b65d78
Hunter Kalinoski <hunterkalinoski@gmail.com>
2023-12-02 15:07:06 -0500

    day 2 optimized regex

--- 2023/2.py
+++ 2023/2.py
@@ -4,17 +4,13 @@ import re
 file_name = os.path.join(os.path.dirname(__file__), "2.txt")
 with open(file_name, "r") as f:
     contents = f.read()
-    p1 = 0
-    p2 = 0
+    p1, p2 = 0, 0
     for i,line in enumerate(contents.split("\n")):
-        maxes = {"red": 0, "green": 0, "blue": 0}
-        line = line[line.find(":")+1:]
-        words = re.findall("\w+", line)
-        for j in range(0, len(words), 2):
-            w = words[j+1]
-            maxes[w] = max(maxes[w], int(words[j]))
-        if maxes['red'] <= 12 and maxes['green'] <= 13 and maxes['blue'] <= 14:
+        maxes = {"r": 0, "g": 0, "b": 0}
+        for num, c in re.findall(r'(\d+) (\w)', line):
+            maxes[c] = max(maxes[c], int(num))
+        if maxes['r'] <= 12 and maxes['g'] <= 13 and maxes['b'] <= 14:
             p1 += i+1
-        p2 += maxes['red'] * maxes['green'] * maxes['blue']
+        p2 += maxes['r'] * maxes['g'] * maxes['b']
     print(p1)
     print(p2)
